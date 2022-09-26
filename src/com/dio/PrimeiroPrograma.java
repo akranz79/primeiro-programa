a7824df08105b3f048712e9e89525443e116921e
alexandre.kranz <alexandre.kranz@FTNOTCWB842.FISCALTECH.CWB>
2022-09-26 20:07:47 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 7;
+		int b = 10;
+		System.out.println("A soma de A e B eh "+(a+b));
+
+	}
+
+}
---
+++ src/com/dio/model/Gato.java
@@ -0,0 +1,16 @@
+package com.dio.model;
+
+public class Gato {
+
+	private String nome;
+	private String cor;
+	private int idade;
+
+	public Gato(String nome, String cor, int idade) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+}
